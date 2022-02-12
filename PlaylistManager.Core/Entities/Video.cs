d8618adef8e64ec47c9a2df7f1f0acddea1ddf5f
Jorge Sunao Arai <araisunao@gmail.com>
2022-02-13 10:08:34 +1100

    Initial Structure

---
+++ PlaylistManager.Core/Entities/Video.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PlaylistManager.Core.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlaylistManager.Core.Entities
+{
+    public class Video: BaseEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public string Description { get; set; }
+        public string Thumbnail { get; set; }
+    }
+}
